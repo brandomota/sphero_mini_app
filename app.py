c302cfe8b35744bc44190a979dd63042a66a12fd
brando mota <brandomb@bol.com.br>
2018-12-20 14:30:00 -0400

    initial

---
+++ app.py
@@ -0,0 +1,16 @@
+from sphero_mini import sphero_mini
+import time
+
+MAC_ADDRESS = "F9:26:CC:DE:AD:41"
+
+ 
+cli = sphero_mini(MAC_ADDRESS)
+
+while True:
+    cli.setLEDColor(red=255, green=255, blue=0)
+
+    time.sleep(5)
+
+    cli.setLEDColor()
+
+
